d01bdb052bb7c9425e5d4cf1a49969bfda2a011d
leone <791960492@qq.com>
2021-06-03 01:38:51 +0800

    Adjust parameters to resist anti-crawler mechanism

--- NVDSpider/spiders/NVD.py
+++ NVDSpider/spiders/NVD.py
@@ -18,7 +18,7 @@ class NvdSpider(scrapy.Spider):
     cve_detail_url_prefix = "https://nvd.nist.gov/vuln/detail/"
     google_scholar_url_prefix = "https://scholar.google.com/scholar?q="
     # key word set by user
-    nvd_search_key_word = "asylo" \
+    nvd_search_key_word = "kernel bound" \
         .replace(" ", "+")
 
     def start_requests(self):
